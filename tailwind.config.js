21974b6545bde850dff8a5cf254eaf2ef29b410f
black8848 <fuzixian666@gmail.com>
2025-01-03 18:38:02 +0800

    修改分割线样式

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            hr: {
+              marginTop: theme('spacing.6'),
+              marginBottom: theme('spacing.6'),
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
-}
+};
